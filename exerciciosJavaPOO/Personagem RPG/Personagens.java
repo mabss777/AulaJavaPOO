c084933e8298ee4bab791ffed2d4311afd5d0dad
mabss777 <mabssdugrauu07@gmail.com>
2025-07-11 11:44:23 -0300

    Desafio Personagem de RPG.

---
+++ exerciciosJavaPOO/Personagem RPG/Personagens.java
@@ -0,0 +1,33 @@
+public class Personagens {
+    public String nome;
+    public String classe;
+    int nivel = 1;
+    int vida = 0;
+    int mana = 0;
+    int forca;
+
+
+    public void atacar(){
+         
+    }
+
+    public void receberDano(int dano){
+
+    }
+
+    public void usarHabilidadeEspecial(){
+
+    }
+
+    public void mostrarStatus(){
+
+    }
+
+    public void subirDeNivel(){
+
+    }
+
+    public void desenharPersonagem(int personagem){
+
+    } 
+}
---
+++ exerciciosJavaPOO/Personagem RPG/ProgramaRPG.java
@@ -0,0 +1,14 @@
+public class ProgramaRPG {
+    public static void main(String[] args) {
+
+    Personagens personagem1 = new Personagens();
+
+
+        
+
+    
+
+        
+    }
+    
+}
